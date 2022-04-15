0e9e4bbd110f63d5bfeb1523a3bfed69e51e6a2c
monojw <monojw.io@gmail.com>
2022-04-15 20:22:24 +0900

    .

--- assets/js/bxslider.js
+++ assets/js/bxslider.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const a = $('.bxslider').bxSlider({
+  const bxSlider = $('.bxslider').bxSlider({
     auto: true,
     stopAutoOnClick: false,
     slideWidth: 600,
@@ -9,28 +9,45 @@ $(document).ready(function () {
     controls: false,
     autoControls: false,
     autoStart: true,
-    infiniteLoop: true,
+    infiniteLoop: false,
     speed: 700,
     prevSelector: '.prev-btn',
     nextSelector: '.next-btn',
+    pagerCustcom: '.pager',
 
+    /* 슬라이드 로드되자마자 실행 */
     onSliderLoad: function () {
-      $('.bxslider > li').eq(0).addClass('active-slide');
+      $('.bxslider > li').eq(0).addClass('active-slide'); // 첫 번째 <li> 클래스 지정한다.
     },
 
+    /* 슬라이드가 전환되기전 실행 */
     onSlideBefore: function ($slideElement) {
-      $('.bxslider > li').removeClass('active-slide');
-      $slideElement.addClass('active-slide');
+      $('.bxslider > li').removeClass('active-slide'); // 기존 <li> 클래스를 지우고
+      $slideElement.addClass('active-slide'); // 다음 <li> 클래스 지정한다.
     },
   });
 
+  /* prev버튼 클릭시 이벤트 */
   $('.prev-btn').click(function () {
-    a.goToPrevSlide();
+    bxSlider.goToPrevSlide(); // 이전 슬라이드로 이동한다.
     return false;
   });
 
+  /* next버튼 클릭시 이벤트 */
   $('.next-btn').click(function () {
-    a.goToNextSlide();
+    bxSlider.goToNextSlide(); // 다음 슬라이드로 이동한다.
+
     return false;
+  });
+
+  /* pager버튼 클릭시 이벤트 */
+  $('.pager a').click(function (e) {
+    e.preventDefault();
+
+    /*  let current = $(e.currentTarget);
+    let i = parseInt(current.attr('data-slide-index'));
+    if ($(this).attr('data-slide-index') !== undefined) {
+      bxSlider.goToSlide(i);
+    } */
   });
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,6 @@
-/* 공지사항 슬라이더 */
+/* 공지사항 슬라이드 */
 const rolling = document.querySelector('.notice-rolling ul');
 const lists = document.querySelectorAll('.notice-rolling li');
-
 let i = 0;
 
 setInterval(function () {
@@ -11,10 +10,11 @@ setInterval(function () {
   } else {
     rolling.style.top = -i * 24 + 'px';
   }
-}, 1000);
+}, 1500);
 
-/*  */
-document.querySelector('.notice .prom-btn').addEventListener('click', (e) => {
+/* 스타벅스 프로모션 버튼, 클릭 이벤트 */
+const promBtn = document.querySelector('.notice .prom-btn');
+promBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   const current = e.currentTarget;
@@ -24,34 +24,36 @@ document.querySelector('.notice .prom-btn').addEventListener('click', (e) => {
   prom.classList.toggle('prom-show');
 });
 
-/* GNB메뉴 마우스 이벤트 */
+/* GNB 메뉴, 마우스 이벤트 */
 const gnbSubWrap = document.querySelector('.gnb_sub_wrap');
-
 document.querySelectorAll('.gnb_bottom_inner > ul > li').forEach((v, i) => {
+  // 마우스를 올리면 메뉴가 나타난다.
   v.addEventListener('mouseover', (e) => {
     const subInner = v.querySelector('.gnb_sub_wrap');
     subInner.style.maxHeight = subInner.scrollHeight + 'px';
   });
 
+  // 마우스가 나가면 메뉴가 사라진다.
   v.addEventListener('mouseout', (e) => {
     const subInner = v.querySelector('.gnb_sub_wrap');
     subInner.style.maxHeight = 0;
   });
 });
 
-document
-  .querySelector('.gnb_top .btn_search')
-  .addEventListener('click', (e) => {
-    const current = e.currentTarget;
-    current.classList.toggle('show');
+/* 검색버튼, 클릭 이벤트 */
+const btnSearch = document.querySelector('.gnb_top .btn_search');
+btnSearch.addEventListener('click', (e) => {
+  const current = e.currentTarget;
+  current.classList.toggle('show');
 
-    if (current.classList.contains('show')) {
-      show(e);
-    } else {
-      hide(e);
-    }
-  });
+  if (current.classList.contains('show')) {
+    show(e);
+  } else {
+    hide(e);
+  }
+});
 
+// <input>를 보여준다.
 function show(e) {
   const current = e.currentTarget;
   current.style.width = '180px';
@@ -64,6 +66,7 @@ function show(e) {
   input.style.display = 'block';
 }
 
+// <input>을 숨긴다.
 function hide(e) {
   const current = e.currentTarget;
   current.style.width = '34px';
@@ -76,7 +79,7 @@ function hide(e) {
   input.style.display = 'none';
 }
 
-/* 스크롤시 이벤트, 애니메이션  */
+/* 스크롤시 발생하는 이벤트, 애니메이션을 적용한다. */
 const scroll = document.querySelectorAll('#scroll');
 const windowHeight = window.innerHeight;
 
@@ -84,7 +87,9 @@ function scrollAnimations() {
   scroll.forEach((v, i) => {
     const scrollTop = v.getBoundingClientRect().top;
 
+    // 윈도우의 높이가 아이템의 top 값보다 크면
     if (windowHeight > scrollTop) {
+      // 클래스를 추가한다.
       v.classList.add('animations');
     } else {
       v.classList.remove('animations');
